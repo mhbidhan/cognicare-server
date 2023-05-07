76927b5dbb2f5446d1ae95d89f6d5734d7eab25c
mahamud <mhbidhan5@gmail.com>
2023-05-07 11:12:52 +0600

    Defined a model for patient

---
+++ src/models/patients/patients.model.js
@@ -0,0 +1,90 @@
+const { Schema, model } = require('mongoose');
+
+const patientSchema = new Schema({
+  caretaker: {
+    tpye: Schema.Types.ObjectId,
+    ref: 'Caretaker',
+    required: true,
+  },
+  name: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  age: {
+    type: Number,
+    min: 1,
+    max: 200,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  loginCode: {
+    type: String,
+    min: 3,
+    max: 255,
+    required: true,
+  },
+  emergencyContact: {
+    _id: false,
+    name: {
+      type: String,
+      min: 3,
+      max: 255,
+      required: true,
+    },
+    phone: {
+      type: String,
+      min: 3,
+      max: 255,
+      required: true,
+    },
+    relation: {
+      type: String,
+      min: 3,
+      max: 255,
+      required: true,
+    },
+  },
+  contacts: {
+    type: [
+      {
+        _id: false,
+        name: {
+          type: String,
+          min: 3,
+          max: 255,
+          required: true,
+        },
+        phone: {
+          type: String,
+          min: 3,
+          max: 255,
+          required: true,
+        },
+        relation: {
+          type: String,
+          min: 3,
+          max: 255,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+    default: [],
+  },
+  // routine: {
+  //   type: Schema.Types.ObjectId,
+  //   ref:'Patient-Routine',
+  //   required: false,
+  // },
+});
+
+const patients = model('Patient', patientSchema);
+
+module.exports = patients;
