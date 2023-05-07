4e62bae9006f6aec98e0e4887e0f104c7715b2f7
mahamud <mhbidhan5@gmail.com>
2023-05-07 11:31:41 +0600

    Added Cloudinary to the project and environment variables for it

--- src/config.js
+++ src/config.js
@@ -4,4 +4,7 @@ module.exports = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT_KEY: process.env.JWT_KEY,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
