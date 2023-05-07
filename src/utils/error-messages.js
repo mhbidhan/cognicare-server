498ff4caf1a38e96bb5f47d6d156eaab9872f090
mahamud <mhbidhan5@gmail.com>
2023-05-07 10:03:01 +0600

    Defined error messages module to store all the common error messages

---
+++ src/utils/error-messages.js
@@ -0,0 +1,7 @@
+const errorMessages = {
+  notFound: 'data not found',
+  alreadyExists: 'already exists',
+  invalidLogin: 'The email or password is invalid',
+};
+
+module.exports = errorMessages;
