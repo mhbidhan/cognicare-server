596eb3343d301b2942c2a609fd3082f19bfb8123
mahamud <mhbidhan5@gmail.com>
2023-05-07 16:19:09 +0600

    Defined router for patients and added it to the main app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const caretakersRouter = require('./routers/caretakers/caretaker.routes');
 const caretakerLoginsRouter = require('./routers/caretakerLogins/caretakerLogins.routes');
+const patientRouter = require('./routers/patients/patients.routes');
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(
 // Routers
 app.use('/caretakers/login', caretakerLoginsRouter);
 app.use('/caretakers', caretakersRouter);
+app.use('/patients', patientRouter);
 
 module.exports = app;
