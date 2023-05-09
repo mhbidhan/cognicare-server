8784b3ac0912b93f7bc7da068ead3ddfacd75cf8
mahamud <mhbidhan5@gmail.com>
2023-05-09 09:46:45 +0600

    Modified error message module to add a new error message

--- src/utils/error-messages.js
+++ src/utils/error-messages.js
@@ -2,6 +2,7 @@ const errorMessages = {
   notFound: 'data not found',
   alreadyExists: 'already exists',
   invalidLogin: 'The email or password is invalid',
+  badRequest: 'bad request',
 };
 
 module.exports = errorMessages;
