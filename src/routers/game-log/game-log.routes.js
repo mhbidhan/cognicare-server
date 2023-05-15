dec310654fd2fa9eda30c176a9fe8d3f83fcf32a
mahamud <mhbidhan5@gmail.com>
2023-05-15 15:04:33 +0600

    Modified the game router to fix the invalid path

--- src/routers/game-log/game-log.routes.js
+++ src/routers/game-log/game-log.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
   getAllGameLogs,
   createNewGameLog,
-} = require('../../controllers/game-log/gmae-log.controller');
+} = require('../../controllers/game-log/game-log.controller');
 
 const gameLogRouter = express.Router();
 
