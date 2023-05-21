6cf2915e2c74820d30a966eb1d3ced90601fe994
mahamud <mhbidhan5@gmail.com>
2023-05-21 12:33:53 +0600

    Defined and utility function to send notifications to the patient

---
+++ src/utils/patient-notification.js
@@ -0,0 +1,48 @@
+const moment = require('moment');
+const patientRoutines = require('../models/patient-routine/patient-routine.model');
+
+async function sendTaskNotificationToPatient(connectedUsers, io) {
+  try {
+    const users = Object.keys(connectedUsers);
+    if (!users.length) return;
+
+    const allRoutines = await patientRoutines
+      .find({
+        $or: users.map((user) => ({
+          patient: user,
+        })),
+      })
+      .populate('routineElements');
+
+    if (!allRoutines.length) return;
+
+    const routineElements = allRoutines
+      .map((routine) =>
+        routine.routineElements.map((routineElement) => {
+          routineElement.patient = routine.patient;
+
+          return routineElement;
+        })
+      )
+      .flat();
+
+    if (!routineElements.length) return;
+
+    const time = moment(new Date()).format('LT');
+
+    for (let routineElement of routineElements) {
+      if (routineElement.startTime.timeInString === time) {
+        io.to(connectedUsers[routineElement.patient]).emit('taskReminder', {
+          type: routineElement.activityType,
+          time,
+          message: routineElement.name,
+          details: routineElement,
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = sendTaskNotificationToPatient;
